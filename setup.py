d7878676dec23b459e039205c1edad2e526dd67c
Zed Pobre <zed@resonant.org>
2013-06-11 18:24:16 -0400

    Imported Upstream version 0.4.12

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 import sys, os, getopt
 from distutils.core import setup, Extension
 
-version_string = "0.4.11"
+version_string = "0.4.12"
 
-if sys.version_info[:2] < (2,4):
-	print "Sorry, duplicity requires version 2.4 or later of python"
+if sys.version_info[:2] < (2,3):
+	print "Sorry, duplicity requires version 2.3 or later of python"
 	sys.exit(1)
 
 try:
