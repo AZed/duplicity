cdda44a944b54f1c2a889b1d211894dcac3bc35a
Zed Pobre <zed@resonant.org>
2013-06-11 18:20:19 -0400

    Imported Upstream version 0.4.4~rc1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import sys, os, getopt
 from distutils.core import setup, Extension
 
-version_string = "0.4.3"
+version_string = "0.4.4.RC1"
 
 if sys.version_info[:2] < (2,2):
 	print "Sorry, duplicity requires version 2.2 or later of python"
@@ -17,7 +17,20 @@ except ImportError:
 
 if not pexpect_version or pexpect_version < "2.1":
 	print ("Warning: pexpect version 2.1 or greater is required for the ssh backend.\n"
-		   "         If you do not plan to use the ssh backend, this is not a problem.")
+		   "		 If you do not plan to use the ssh backend, this is not a problem.")
+
+incdir_list = libdir_list = None 
+
+if os.name == 'posix':
+	LIBRSYNC_DIR = os.environ.get('LIBRSYNC_DIR', '')
+	args = sys.argv[:]
+	for arg in args:
+		if arg.startswith('--librsync-dir='):
+			LIBRSYNC_DIR = arg.split('=')[1]
+			sys.argv.remove(arg)
+	if LIBRSYNC_DIR:
+		incdir_list = [os.path.join(LIBRSYNC_DIR, 'include')]
+		libdir_list = [os.path.join(LIBRSYNC_DIR, 'lib')]
 
 setup(name="duplicity",
 	  version=version_string,
@@ -31,6 +44,8 @@ setup(name="duplicity",
 	  package_dir = {"duplicity": "src"},
 	  ext_modules = [Extension("duplicity._librsync",
 							   ["_librsyncmodule.c"],
+							   include_dirs=incdir_list,
+							   library_dirs=libdir_list,
 							   libraries=["rsync"])],
 	  scripts = ['rdiffdir', 'duplicity'],
 	  data_files = [('share/man/man1', ['duplicity.1', 'rdiffdir.1']),
