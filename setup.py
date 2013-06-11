b4a4ac96c0d90dac42aa7ad5f1156e1180e35d18
Zed Pobre <zed@resonant.org>
2013-06-11 18:25:30 -0400

    Imported Upstream version 0.5.05

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@
 
 import sys, os, getopt
 from distutils.core import setup, Extension
-from DistUtilsExtra.command import *
 
-version_string = "0.5.04"
+version_string = "0.5.05"
 
 if sys.version_info[:2] < (2,3):
     print "Sorry, duplicity requires version 2.3 or later of python"
@@ -21,7 +20,7 @@ if not pexpect_version or pexpect_version < "2.1":
     print ("Warning: pexpect version 2.1 or greater is required for the ssh backend.\n"
            "         If you do not plan to use the ssh backend, this is not a problem.")
 
-incdir_list = libdir_list = None 
+incdir_list = libdir_list = None
 
 if os.name == 'posix':
     LIBRSYNC_DIR = os.environ.get('LIBRSYNC_DIR', '')
@@ -34,6 +33,28 @@ if os.name == 'posix':
         incdir_list = [os.path.join(LIBRSYNC_DIR, 'include')]
         libdir_list = [os.path.join(LIBRSYNC_DIR, 'lib')]
 
+data_files = [('share/man/man1',
+               ['duplicity.1',
+                'rdiffdir.1']),
+              ('share/doc/duplicity-%s' % version_string,
+               ['COPYING',
+                'CVS-README',
+                'LOG-README',
+                'README',
+                'tarfile-LICENSE',
+                'CHANGELOG']),
+              ]
+
+assert os.path.exists("po"), "Missing 'po' directory."
+for root, dirs, files in os.walk("po"):
+    for file in files:
+        path = os.path.join(root, file)
+        if path.endswith("duplicity.mo"):
+            lang = os.path.split(root)[-1]
+            data_files.append(
+                ('share/locale/%s/LC_MESSAGES' % lang,
+                 ["po/%s/duplicity.mo" % lang]))
+
 setup(name="duplicity",
       version=version_string,
       description="Encrypted backup using rsync algorithm",
@@ -42,19 +63,15 @@ setup(name="duplicity",
       maintainer="Kenneth Loafman",
       maintainer_email="kenneth@loafman.com",
       url="http://duplicity.nongnu.org/index.html",
-      cmdclass = { "build" : build_extra.build_extra,
-                   "build_i18n" :  build_i18n.build_i18n },
-      packages = ['duplicity', 'duplicity.backends'],
+      packages = ['duplicity',
+                  'duplicity.backends',],
       package_dir = {"duplicity" : "src",
-                     "duplicity.backends" : "src/backends"},
+                     "duplicity.backends" : "src/backends",},
       ext_modules = [Extension("duplicity._librsync",
                                ["_librsyncmodule.c"],
                                include_dirs=incdir_list,
                                library_dirs=libdir_list,
                                libraries=["rsync"])],
       scripts = ['rdiffdir', 'duplicity'],
-      data_files = [('share/man/man1', ['duplicity.1', 'rdiffdir.1']),
-                    ('share/doc/duplicity-%s' % version_string,
-                     ['COPYING', 'README', 'CHANGELOG'])])
-
-
+      data_files = data_files,
+      )
