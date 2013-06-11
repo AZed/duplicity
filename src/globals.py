d49d189a76dbf7e65b683239ff4eb13c9b686e79
Zed Pobre <zed@resonant.org>
2013-06-11 18:26:20 -0400

    Imported Upstream version 0.5.09

--- src/globals.py
+++ src/globals.py
@@ -24,7 +24,7 @@
 import socket, sys
 
 # The current version of duplicity
-version = "0.5.08"
+version = "0.5.09"
 
 # The name of the current host, or None if it cannot be set
 hostname = socket.getfqdn()
